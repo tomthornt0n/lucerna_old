7c28a9eb90659b85b474b8000797a1825445e004
Tom Thornton <thomas.thornton40@gmail.com>
2020-07-23 13:00:54 +0100

    Added lcddl as a subtree

--- lcddl/lcddlUserLayer.c
+++ lcddl/lcddlUserLayer.c
@@ -1,55 +1,210 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "lcddl.h"
 
-/* LCDDL - 'lucerna data description language'
-   
-   lcddl is a simple utility which parses a minimal data description format.
-   Once a data layout has been extracted from the input file, a corresponding
-   tree is constructed and sent to the user code.
-   
-   The tree is arranged as a linked list of 'top level' nodes, each of which
-   contains a Name, a Type, a IndirectionLevel, a pointer, which may be NULL,
-   to a linked list of child nodes, and a pointer, which may be NULL, to a
-   linked list of annotations.
+FILE *ComponentsFile = NULL;
+FILE *FunctionsFile = NULL;
+FILE *ComponentArraysFile = NULL;
+FILE *AllocationsFile = NULL;
+FILE *DeallocationsFile = NULL;
 
-   lcddlNode_t is defined as:
+int bitOffset = 1;
 
-    typedef struct lcddlNode_t lcddlNode_t;
-    struct lcddlNode_t
+void fWriteStringAsUppercaseWithUnderscores(FILE *file, char *string)
+{
+    int stringLength = 0;
+    for (; string[stringLength]; ++stringLength);
+
+    int i;
+    for(i = 0; i < stringLength && string[i]; ++i)
     {
-         char Name[32];
-         char Type[32];
-         int IndirectionLevel;
-    
-         lcddlNode_t *Next;
-         lcddlNode_t *Children;
-    
-         lcddlAnnotation_t *Tags;
-    };
+        fprintf(file, "%c",
+               (string[i] >= 'a' && string[i] <= 'z') ?
+                string[i] - 32 : string[i]);
 
-   lcddlAnnotation_t is defined as:
-   
-    typedef struct lcddlAnnotation_t lcddlAnnotation_t;
-    struct lcddlAnnotation_t
+        if(string[i+1] >= 'A' && string[i+1] < 'Z' && string[i] != '_')
+            fprintf(file, "_");
+    }
+}
+
+void fWriteStringAsLowercaseWithUnderscores(FILE *file, char *string)
+{
+    int stringLength = 0;
+    for (; string[stringLength]; ++stringLength);
+
+    int i;
+    for (i = 0; i < stringLength && string[i]; ++i)
     {
-        char Tag[32];
-        lcddlAnnotation_t *Next;
-    };
-*/
+        fprintf(file, "%c",
+               (string[i] >= 'A' && string[i] <= 'Z') ?
+                string[i] + 32 : string[i]);
+
+        if (string[i+1] >= 'A' && string[i+1] < 'Z' && string[i] != '_')
+            fprintf(file, "_");
+    }
+}
+
+void fWriteStringAsTitleCase(FILE *file, char *string)
+{
+    int stringLength = 0;
+    for (; string[stringLength]; ++stringLength);
+
+    int needsUppercase = 1;
+
+    int i;
+    for (i = 0; i < stringLength && string[i]; ++i)
+    {
+        if (string[i] != '_')
+        {
+            if (string[i - 1] == '_')
+                needsUppercase = 1;
+        }
+
+        if (needsUppercase)
+        {
+            fprintf(file, "%c",
+                   (string[i] >= 'a' && string[i] <= 'z') ?
+                    string[i] - 32 : string[i]);
+            needsUppercase = 0;
+        }
+        else if (string[i] != '_')
+        {
+            fprintf(file, "%c", string[i]);
+        }
+    }
+}
+
+void GenerateComponent(lcddlNode_t *component)
+{
+    fprintf(stderr, "        Generating component struct...\n");
+    fprintf(ComponentsFile, "#define LC_COMPONENT_");
+    fWriteStringAsUppercaseWithUnderscores(ComponentsFile, component->Name);
+    fprintf(ComponentsFile, " BIT(%i)\n", bitOffset++);
+    if(bitOffset == 0xffffffffffffffff)
+    {
+        fprintf(stderr, "\033[31mERROR: Too many components!\033[0m\n");
+        exit(-1);
+    }
+    lcddl_WriteStructToFileAsC(ComponentsFile, component);
+
+    fprintf(stderr, "        Creating component array...\n");
+    fprintf(ComponentArraysFile, "    %s *", component->Name);
+    fWriteStringAsTitleCase(ComponentArraysFile, component->Name);
+    fprintf(ComponentArraysFile, ";\n");
+    fprintf(AllocationsFile, "scene.");
+    fWriteStringAsTitleCase(AllocationsFile, component->Name);
+    fprintf(AllocationsFile,
+            " = malloc(LC_MAX_ENTITIES * sizeof(%s));\n",
+            component->Name);
+    fprintf(DeallocationsFile,
+            "    free(scene->");
+    fWriteStringAsTitleCase(DeallocationsFile, component->Name);
+    fprintf(DeallocationsFile, ");\n");
+
+    fprintf(stderr, "        Generating add function...\n\n");
+
+    fprintf(FunctionsFile, "void lc_Add");
+    fWriteStringAsTitleCase(FunctionsFile, component->Name);
+    fprintf(FunctionsFile, "(lcScene_t* scene, lcEntity_t entity");
+    lcddlNode_t *member = component->Children;
+    while (member)
+    {
+        int pointerCount = member->IndirectionLevel;
+        fprintf(FunctionsFile,
+                ", %s ", member->Type);
+        int i;
+        for (i = 0; i < pointerCount; ++i)
+            fprintf(FunctionsFile, "*");
+        fWriteStringAsLowercaseWithUnderscores(FunctionsFile, member->Name);
+        member = member->Next;
+    }
+    fprintf(FunctionsFile,
+            ")\n{\n    scene->EntitySignatures[entity] |= LC_COMPONENT_");
+    fWriteStringAsUppercaseWithUnderscores(FunctionsFile,
+                                           component->Name);
+    fprintf(FunctionsFile, ";\n");
+
+    member = component->Children;
+    while (member)
+    {
+        fprintf(FunctionsFile,
+                "    scene->");
+        fWriteStringAsTitleCase(FunctionsFile,
+                                component->Name);
+        fprintf(FunctionsFile,
+                "[entity].%s = ", member->Name);
+        fWriteStringAsLowercaseWithUnderscores(FunctionsFile,
+                                               member->Name);
+        fprintf(FunctionsFile, ";\n");
+        member = member->Next;
+    }
+    fprintf(FunctionsFile, "}\n\n");
+}
 
 void lcddlUserInitCallback(void)
 {
-    /* Called once at when lcddl is run. */
+    fprintf(stderr,
+            "\n    \033[34mInitialising LCDDL...\033[0m\n\n");
+
+    ComponentsFile = fopen(
+            "Engine/Source/ECS/Components.gen.c",
+            "w");
+    FunctionsFile = fopen(
+        "Engine/Source/ECS/EcsFunctions.gen.c",
+        "w");
+    ComponentArraysFile = fopen(
+        "Engine/Source/ECS/ComponentArrays.gen.c",
+        "w");
+    AllocationsFile = fopen(
+        "Engine/Source/ECS/ComponentArraysAllocation.gen.c",
+        "w");
+    DeallocationsFile = fopen(
+        "Engine/Source/ECS/lc_SceneDestroy.gen.c",
+        "w");
+
+    if (!ComponentArraysFile ||
+        !FunctionsFile       ||
+        !ComponentArraysFile)
+    {
+        fprintf(stderr, "\033[31mError opening output files!\033[0m");
+        exit(-1);
+    }
+
+    fprintf(ComponentsFile,
+            "#ifndef COMPONENTS_H\n#define COMPONENTS_H\n\n");
+    fprintf(ComponentsFile,
+            "#define LC_COMPONENT_NONE 0\n\n");
+
+    fprintf(DeallocationsFile,
+            "void lc_SceneDestroy(lcScene_t *scene)\n{\n");
 }
 
 void lcddlUserTopLevelCallback(lcddlNode_t *node)
 {
-    /* Called for every 'top level' node */
+    if (node->Children &&
+        lcddl_NodeHasTag(node, "Component"))
+    {
+        fprintf(stderr,
+                "    \033[34mLCDDL parse function for %s\033[0m\n", node->Name);
+        GenerateComponent(node);
+    }
 }
 
 void lcddlUserCleanupCallback(void)
 {
-    /* called just before the program exits */
-}
+    fprintf(stderr, "    \033[34mFinishing up...\n\n");
 
+    fprintf(ComponentsFile, "#endif\n");
+    fprintf(DeallocationsFile, "}\n");
+
+    fclose(ComponentsFile);
+    fclose(FunctionsFile);
+    fclose(ComponentArraysFile);
+    fclose(AllocationsFile);
+    fclose(DeallocationsFile);
+}
